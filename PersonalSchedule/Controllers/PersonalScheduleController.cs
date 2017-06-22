8e75cb0a8b75a148d844971ef5e77a6dd923aceb
flyboss <915741005@qq.com>
2017-06-23 00:09:34 +0800

    增加了共享程序集

--- PersonalSchedule/Controllers/PersonalScheduleController.cs
+++ PersonalSchedule/Controllers/PersonalScheduleController.cs
@@ -10,6 +10,7 @@ using SeviceLib;
 using PersonalSchedule.Models;
 using EncryptLib;
 using ToolsLib;
+using LIWeiYINYifanLib;
 
 namespace PersonalSchedule.Controllers
 {
@@ -68,7 +69,19 @@ namespace PersonalSchedule.Controllers
         }
         private string Register(string register_id, string register_pwd, string register_name = null, string register_auth = "usr")
         {
-            return PersonalScheduleService.Register(db, register_id, register_pwd, register_name, register_auth);
+            string message = PersonalScheduleService.Register(db, register_id, register_pwd, register_name, register_auth);
+            if (message.Equals("注册成功"))
+            {
+                try
+                {
+                    System.IO.File.Copy(Server.MapPath(HeaderInit.FormFilePath("~/img/default/default-user-", 2, ".png")), Server.MapPath("~/img/user/" + register_id + "_.png"), true);
+                }
+                catch (Exception e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e);
+                }
+            }
+            return message;
         }
         private string Logout()
         {
@@ -92,7 +105,7 @@ namespace PersonalSchedule.Controllers
                 }
                 return currentAddress;
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 System.Diagnostics.Debug.Write(e);
                 return null;
@@ -118,14 +131,14 @@ namespace PersonalSchedule.Controllers
         }
 
         // WebPage: Cal
-        
+
         public ActionResult Cal()
         {
             if (Session["login_id"] == null)
             {
                 return Redirect("/PersonalSchedule/Index");
             }
-            string login_id = Session["login_id"].ToString();            
+            string login_id = Session["login_id"].ToString();
             ViewBag.cal_output_json = PersonalScheduleService.GetCalActionsByUId(db, login_id);//"[{\"date\":\"2017-05-14\",\"title\":\"SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSingle Day Event\",\"time\":\"10:00-12:00\",\"itemid\":\"000\"},{\"date\":\"2017-05-23\",\"title\":\"Single Day Event\",\"time\":\"2\",\"itemid\":\"000\"},{\"date\":\"2017-05-17\",\"title\":\"Single Day Event\",\"time\":\"3\",\"itemid\":\"000\"},{\"date\":\"2017-05-26\",\"title\":\"Single Day Event1\",\"time\":\"4\",\"itemid\":\"000\"},{\"date\":\"2017-05-28\",\"title\":\"Single Day Event3\",\"time\":\"6\",\"itemid\":\"000\"},{\"date\":\"2017-05-26\",\"title\":\"Single Day Event2\",\"time\":\"5\",\"itemid\":\"000\"},{\"date\":\"2017-06-06\",\"title\":\"Single Day Event2\",\"time\":\"7\",\"itemid\":\"000\"},{\"date\":\"2017-06-08\",\"title\":\"Single Day Event2\",\"time\":\"8\",\"itemid\":\"000\"}]";
             return View();
         }
